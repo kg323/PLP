8f668ee5ac4e3c404c2795902e0808956b0faa42
kg323 <kenneth.galindo.231@my.csun.edu>
2022-06-18 12:44:40 -0700

    Updated Repo

---
+++ plptool5/run-windows.bat
@@ -0,0 +1,1 @@
+java -Djava.library.path=. -jar PLPToolStatic.jar -W -D .\extensions %1 %2 %3 %4 %5 %6 %7 %8 %9
---
+++ plptool5/run.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+java -jar /usr/lib/plptool5/PLPToolStatic.jar -W -D ./extensions $@
